1079e5f983f4de6e756414f720f454802fc1405e
caio <caioiwakawa@gmail.com>
2025-06-26 11:41:25 -0300

    feat: caxinha de materia implementada

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,9 +1,11 @@
+import FeedBox from "@/components/feed_box";
 import Header from "@/components/header";
 
 export default function Feed() {
   return (
     <main>
       <Header/>
+      <FeedBox titulo="CIC 0099 -  Organização e Arquitetura de Computadores"/>
     </main>
     );
 }
